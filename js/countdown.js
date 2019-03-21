3e86aae25852514fae966dc33d87e1f325b7e6e4
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-03-21 13:09:57 -0600

    Create prize_page.html and change index.html to main.html

---
+++ js/countdown.js
@@ -0,0 +1,14 @@
+document.getElementById("home_audio").play();
+var countDownDate = new Date("Mar 23 , 2019 03:26:10").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("demo").innerHTML = "Next Game In: "+hours + "h " + minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
