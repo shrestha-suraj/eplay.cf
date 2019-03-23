b1c68819376edadf3821a8c85040846525b92e52
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-03-22 21:54:48 -0500

    Modify main.html anf countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -6,7 +6,7 @@ var x = setInterval(function () {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("demo").innerHTML = "Next Game In: "+hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById("demo").innerHTML = "Next Game In <br/>&nbsp&nbsp&nbsp"+hours + "h " + minutes + "m " + seconds + "s ";
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "EXPIRED";
