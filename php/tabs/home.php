0ed0ec79bdc4e16418b4b0112e31d22408016acc
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-04-10 14:08:19 -0600

    Home Page Done

--- php/tabs/home.php
+++ php/tabs/home.php
@@ -10,9 +10,11 @@
     <link href="../../css/home.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../css/home.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
-<body style="margin:0;">
+<body style="margin:0;background-image:url('../../html/bodywall.jpg');">
     <div>
     <?php 
         include("../../html/header.html");
