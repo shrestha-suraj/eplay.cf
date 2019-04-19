d1f1b925dc7547c6cc74204badf6235e0d5372cf
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-04-19 03:30:30 -0500

    Login and Signup Backend Done

--- php/authentication/login.php
+++ php/authentication/login.php
@@ -1,13 +1,12 @@
 <?php
     session_start();
     $login_email=$_POST['login_email'];
-    $_SESSION["user_email"]='$login_email';
     $login_password=$_POST['login_password'];
     $db=mysqli_connect("localhost","shresthasuraj62","ilovepubg99$","pubg");
-    $query="SELECT * FROM players WHERE email='$login_email' AND passowrd='$login_password'";
+    $query="SELECT * FROM players WHERE Email='$login_email' AND Password='$login_password'";
     $result=mysqli_query($db,$query);
     if(mysqli_num_rows($result)==0){ 
-        header('Location: ../../index.php?access=0');
+        header('Location: ../../index.php?accessErr=0');
     }
     else{
         header('Location: ../tabs/home.php');
--- php/authentication/signup.php
+++ php/authentication/signup.php
@@ -3,15 +3,16 @@
     $pubg_id=$_POST['signup_id'];
     $signup_email=$_POST['signup_email'];
     $signup_password=$_POST['signup_password'];
+    $country_name=$_POST['signup_country'];
     $db=mysqli_connect("localhost","shresthasuraj62","ilovepubg99$","pubg");
-    $query1="SELECT * FROM players WHERE email='$signup_email'";
+    $query1="SELECT * FROM players WHERE Email='$signup_email' OR Pubg_Name='$pubg_id'";
     $result=mysqli_query($db,$query1);
     if(mysqli_num_rows($result)==0){ 
-        $query2="INSERT INTO players(name,user_name,email,passowrd) VALUES('$user_name','$pubg_id','$signup_email','$signup_password')";
+        $query2="INSERT INTO players(Pubg_Name,Name,Email,Password,Country) VALUES('$pubg_id','$user_name','$signup_email','$signup_password','$country_name')";
         $result=mysqli_query($db,$query2);
         header('Location: ../tabs/home.php');
     }
     else{
-        header('Location: ../../index.php');
+        header('Location: ../../index.php?accessErr=1');
     }
 ?>
